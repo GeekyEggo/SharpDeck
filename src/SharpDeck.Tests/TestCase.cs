c8181ac38a3aec41b13ba5c94808a377fcfa71f8
Richard <richard.herman@gmail.com>
2019-02-12 16:46:51 +0000

    Added tests project, #1

---
+++ src/SharpDeck.Tests/TestCase.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpDeck.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Provides a standard test that should always pass.
+    /// </summary>
+    [TestFixture]
+    public class TestCase
+    {
+        /// <summary>
+        /// Tests that <c>true</c> equals <c>true</c>.
+        /// </summary>
+        [TestCase]
+        public void Test()
+            => Assert.True(true);
+    }
+}
