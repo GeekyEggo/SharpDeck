1278bb3c5459ac3372645b7c709c29179fc50242
Richard <richard.herman@gmail.com>
2019-05-28 22:57:16 +0100

    Updated device types

--- src/SharpDeck/Enums/DeviceType.cs
+++ src/SharpDeck/Enums/DeviceType.cs
@@ -13,6 +13,16 @@
         /// <summary>
         /// Defines a Stream Deck Mini: kESDSDKDeviceType_StreamDeckMini.
         /// </summary>
-        StreamDeckMini = 1
+        StreamDeckMini = 1,
+
+        /// <summary>
+        /// Defines a Stream Deck XL: kESDSDKDeviceType_StreamDeckXL.
+        /// </summary>
+        StreamDeckXL = 2,
+
+        /// <summary>
+        /// Defines a Stream Deck Mobile: kESDSDKDeviceType_StreamDeckMobile.
+        /// </summary>
+        StreamDeckMobile = 3
     }
 }
