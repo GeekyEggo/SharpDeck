37f096dfe282fc33ae21f772db03c873ce4dda2f
Richard <richard.herman@gmail.com>
2019-06-07 19:12:48 +0100

    Added options payload

---
+++ src/SharpDeck/PropertyInspectors/Payloads/Option.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace SharpDeck.PropertyInspectors.Payloads
+{
+    /// <summary>
+    /// Provides information about an HTML option.
+    /// </summary>
+    public class Option
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Option"/> class.
+        /// </summary>
+        /// <param name="label">The label.</param>
+        /// <param name="value">The value.</param>
+        public Option(string label, string value)
+        {
+            this.Label = label;
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Option"/> class.
+        /// </summary>
+        /// <param name="label">The label.</param>
+        /// <param name="children">The children.</param>
+        public Option(string label, List<Option> children)
+        {
+            this.Label = label;
+            this.Children = children;
+        }
+
+        /// <summary>
+        /// Gets or sets the children.
+        /// </summary>
+        public List<Option> Children { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the label.
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
---
+++ src/SharpDeck/PropertyInspectors/Payloads/OptionsPayload.cs
@@ -0,0 +1,32 @@
+﻿namespace SharpDeck.PropertyInspectors.Payloads
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides a payload containing options that can serve as a data source for an HTML select input.
+    /// </summary>
+    public class OptionsPayload : PropertyInspectorPayload
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionsPayload"/> class.
+        /// </summary>
+        public OptionsPayload()
+            : this(new List<Option>())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionsPayload"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        public OptionsPayload(List<Option> options)
+        {
+            this.Options = options;
+        }
+
+        /// <summary>
+        /// Gets or sets the options.
+        /// </summary>
+        public List<Option> Options { get; set; }
+    }
+}
