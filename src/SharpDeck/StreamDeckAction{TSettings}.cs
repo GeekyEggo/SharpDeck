e8aa7fe26b013849fb559d976a57158fa01ca860
GeekyEggo <richard.herman@gmail.com>
2019-06-27 23:27:55 +0100

    Added base methods for actions with settings

--- src/SharpDeck/StreamDeckAction{TSettings}.cs
+++ src/SharpDeck/StreamDeckAction{TSettings}.cs
@@ -1,5 +1,6 @@
-﻿namespace SharpDeck
+namespace SharpDeck
 {
+    using SharpDeck.Events.Received;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -22,5 +23,25 @@
         /// <param name="settings">A JSON object which is persistently saved for the action's instance.</param>
         public Task SetSettingsAsync(TSettings settings)
             => this.StreamDeck.SetSettingsAsync(this.Context, settings);
+
+        /// <summary>
+        /// Raises the <see cref="StreamDeckActionEventReceiver.DidReceiveSettings" /> event.
+        /// </summary>
+        /// <param name="args">The <see cref="ActionEventArgs{ActionPayload}" /> instance containing the event data.</param>
+        /// <returns>The task of updating the state of the object based on the settings.</returns>
+        protected override async Task OnDidReceiveSettings(ActionEventArgs<ActionPayload> args)
+        {
+            await this.OnDidReceiveSettings(args, args.Payload.GetSettings<TSettings>());
+            await base.OnDidReceiveSettings(args);
+        }
+
+        /// <summary>
+        /// Handles the <see cref="StreamDeckActionEventReceiver.DidReceiveSettings" /> event.
+        /// </summary>
+        /// <param name="args">The <see cref="ActionEventArgs{TActionPayload}" /> instance containing the event data.</param>
+        /// <param name="settings">The settings.</param>
+        /// <returns>The task of updating the state of the object based on the settings.</returns>
+        protected virtual Task OnDidReceiveSettings(ActionEventArgs<ActionPayload> args, TSettings settings)
+            => Task.CompletedTask;
     }
 }
