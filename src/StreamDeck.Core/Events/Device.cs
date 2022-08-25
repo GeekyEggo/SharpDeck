02438f90674ea1d6426ea430921698bc59133ae4
Richard Herman <geekyeggo@users.noreply.github.com>
2022-08-25 16:43:42 +0100

    Add missing devices

--- src/StreamDeck.Core/Events/Device.cs
+++ src/StreamDeck.Core/Events/Device.cs
@@ -6,28 +6,38 @@ namespace StreamDeck.Events
     public enum Device
     {
         /// <summary>
-        /// Defines a Stream Deck: kESDSDKDeviceType_StreamDeck.
+        /// Stream Deck (kESDSDKDeviceType_StreamDeck).
         /// </summary>
         StreamDeck = 0,
 
         /// <summary>
-        /// Defines a Stream Deck Mini: kESDSDKDeviceType_StreamDeckMini.
+        /// Stream Deck Mini (kESDSDKDeviceType_StreamDeckMini).
         /// </summary>
         StreamDeckMini = 1,
 
         /// <summary>
-        /// Defines a Stream Deck XL: kESDSDKDeviceType_StreamDeckXL.
+        /// Stream Deck XL (kESDSDKDeviceType_StreamDeckXL).
         /// </summary>
         StreamDeckXL = 2,
 
         /// <summary>
-        /// Defines a Stream Deck Mobile: kESDSDKDeviceType_StreamDeckMobile.
+        /// Stream Deck Mobile (kESDSDKDeviceType_StreamDeckMobile).
         /// </summary>
         StreamDeckMobile = 3,
 
         /// <summary>
-        /// Defines a Corsair G-Key compatible keyboard: kESDSDKDeviceType_CorsairGKeys.
+        /// Corsair G-Key compatible keyboard (kESDSDKDeviceType_CorsairGKeys).
         /// </summary>
-        CorsairGKeys = 4
+        CorsairGKeys = 4,
+
+        /// <summary>
+        /// Stream Deck Pedal (kESDSDKDeviceType_StreamDeckPedal).
+        /// </summary>
+        StreamDeckPedal = 5,
+
+        /// <summary>
+        /// Corsair Voyager laptop (kESDSDKDeviceType_CorsairVoyager).
+        /// </summary>
+        CorsairVoyager = 6
     }
 }
