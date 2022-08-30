f96a30db74a803e36160da304b5368c665288705
Richard Herman <geekyeggo@users.noreply.github.com>
2022-08-30 22:45:23 +0100

    Improve coverage helper script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,15 +1,21 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 dotnet test \
+    -p:Exclude=\"[*.Tests]*\" \
+    -p:Excludebyattribute="ExcludeFromCodeCoverage" \
     -p:CollectCoverage=true \
     -p:CoverletOutput="$SCRIPTPATH/../tests/.coverage/" \
-    -p:CoverletOutputFormat=cobertura \
-    -p:Excludebyattribute="ExcludeFromCodeCoverage" \
+    -p:CoverletOutputFormat=\"cobertura,json\" \
+    -p:MergeWith="$SCRIPTPATH/../tests/.coverage/coverage.json" \
+    -l:"console;verbosity=detailed" \
+    --blame \
+    --blame-hang-timeout:"10s"
+    -m:1 \
     --no-build \
-    --verbosity normal
 
- 
 reportgenerator \
     -reports:"$SCRIPTPATH/../tests/.coverage/coverage.cobertura.xml" \
     -targetdir:"$SCRIPTPATH/../tests/.coverage/report" \
     -reporttypes:Html
+
+start "$SCRIPTPATH/../tests/.coverage/report/index.html"
