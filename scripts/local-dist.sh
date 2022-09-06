b5b89ec05c5d40af5217a9f45e93cca2fc72b2ce
Richard Herman <geekyeggo@users.noreply.github.com>
2022-09-06 12:57:45 +0100

    Add helper script for local debug via nuget

---
+++ scripts/local-dist.sh
@@ -0,0 +1,16 @@
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+pushd $SCRIPTPATH/..
+
+dotnet clean
+dotnet pack ./src/StreamDeck/StreamDeck.csproj -c Release -p:Version=$1
+
+# This must be registered as alocal  NuGet source.
+mv ./dist/StreamDeck.* ~/AppData/Local/NuGet/Local/
+
+pushd ./examples/Counter
+dotnet add package StreamDeck -v $1
+dotnet restore
+popd
+
+popd
