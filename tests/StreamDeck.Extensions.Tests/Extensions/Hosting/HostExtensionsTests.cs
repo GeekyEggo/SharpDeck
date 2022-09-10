8aac1ad0ed1a7931d443ba6d1e6251de98a418ac
Richard Herman <geekyeggo@users.noreply.github.com>
2022-09-10 01:28:50 +0100

    Add todo

---
+++ tests/StreamDeck.Extensions.Tests/Extensions/Hosting/HostExtensionsTests.cs
@@ -0,0 +1,12 @@
+namespace StreamDeck.Tests.Extensions.Hosting
+{
+    using StreamDeck.Extensions.Hosting;
+
+    /// <summary>
+    /// Provides assertions for <see cref="HostExtensions"/>.
+    /// </summary>
+    public class HostExtensionsTests
+    {
+        // TODO
+    }
+}
