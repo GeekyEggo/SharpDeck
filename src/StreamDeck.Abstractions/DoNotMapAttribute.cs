34df79633c2a78a33d03c49003552903c77021d0
Richard Herman <geekyeggo@users.noreply.github.com>
2022-09-18 23:33:28 +0100

    Fix attribute usage

--- src/StreamDeck.Abstractions/DoNotMapAttribute.cs
+++ src/StreamDeck.Abstractions/DoNotMapAttribute.cs
@@ -5,6 +5,7 @@ namespace StreamDeck
     /// <summary>
     /// When present, the action is not automatically mapped as part of <see cref="T:StreamDeck.Extensions.Hosting.GeneratedHostExtensions.MapActions(Microsoft.Extensions.Hosting.IHost)"/>
     /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
 #if BUILDING_SOURCE_GENERATOR
     internal
 #else
