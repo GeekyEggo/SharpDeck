cfa61de3ffb9231655415f9df93a751ee5efda05
Richard Herman <geekyeggo@users.noreply.github.com>
2022-09-28 16:31:00 +0100

    Add mapping extension methods to the Host

--- src/StreamDeck.Extensions/Extensions/Hosting/HostExtensions.cs
+++ src/StreamDeck.Extensions/Extensions/Hosting/HostExtensions.cs
@@ -1,6 +1,7 @@
 namespace StreamDeck.Extensions.Hosting
 {
     using System;
+    using System.Text.Json.Nodes;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using StreamDeck.Events;
@@ -41,8 +42,68 @@ namespace StreamDeck.Extensions.Hosting
         }
 
         /// <summary>
-        /// Maps the <paramref name="action"/> to the <see cref="IStreamDeckConnection"/>, with parameters resolved from the <see cref="IServiceProvider"/>.
-        /// <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs{KeyPayload}"/> are propagated directly from the <see cref="IStreamDeckConnection.KeyDown"/> event.
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.ApplicationDidLaunch"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="StreamDeckEventArgs{ApplicationPayload}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.ApplicationDidLaunch"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapApplicationDidLaunch(this IHost host, Delegate action)
+            => host.MapEvent<StreamDeckEventArgs<ApplicationPayload>>(action, (conn, handler) => conn.ApplicationDidLaunch += handler);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.ApplicationDidTerminate"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="StreamDeckEventArgs{ApplicationPayload}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.ApplicationDidTerminate"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapApplicationDidTerminate(this IHost host, Delegate action)
+            => host.MapEvent<StreamDeckEventArgs<ApplicationPayload>>(action, (conn, handler) => conn.ApplicationDidTerminate += handler);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.DeviceDidConnect"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="DeviceConnectEventArgs"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.DeviceDidConnect"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapDeviceDidConnect(this IHost host, Delegate action)
+            => host.MapEvent<DeviceConnectEventArgs>(action, (conn, handler) => conn.DeviceDidConnect += handler);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.DeviceDidDisconnect"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="DeviceEventArgs"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.DeviceDidDisconnect"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapDeviceDidDisconnect(this IHost host, Delegate action)
+            => host.MapEvent<DeviceEventArgs>(action, (conn, handler) => conn.DeviceDidDisconnect += handler);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.DidReceiveGlobalSettings"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="StreamDeckEventArgs{SettingsPayload}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.DidReceiveGlobalSettings"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapDidReceiveGlobalSettings(this IHost host, Delegate action)
+            => host.MapEvent<StreamDeckEventArgs<SettingsPayload>>(action, (conn, handler) => conn.DidReceiveGlobalSettings += handler);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.DidReceiveSettings"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs{ActionPayload}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.DidReceiveSettings"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapDidReceiveSettings(this IHost host, Delegate action)
+            => host.MapEvent<ActionEventArgs<ActionPayload>>(action, (conn, handler) => conn.DidReceiveSettings += handler, args => args.Context);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.KeyDown"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs{KeyPayload}"/> which are propagated directly from the event.
         /// </summary>
         /// <param name="host">The <see cref="IHost"/> to configure.</param>
         /// <param name="action">The action to map to <see cref="IStreamDeckConnection.KeyDown"/>.</param>
@@ -51,12 +112,92 @@ namespace StreamDeck.Extensions.Hosting
             => host.MapEvent<ActionEventArgs<KeyPayload>>(action, (conn, handler) => conn.KeyDown += handler, args => args.Context);
 
         /// <summary>
-        /// Compiles the <paramref name="action"/>, and invokes <paramref name="addHandler"/> allowing the event handler to be added to the <see cref="IStreamDeckConnection"/>.
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.KeyUp"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs{KeyPayload}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.KeyUp"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapKeyUp(this IHost host, Delegate action)
+            => host.MapEvent<ActionEventArgs<KeyPayload>>(action, (conn, handler) => conn.KeyUp += handler, args => args.Context);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.PropertyInspectorDidAppear"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.PropertyInspectorDidAppear"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapPropertyInspectorDidAppear(this IHost host, Delegate action)
+            => host.MapEvent<ActionEventArgs>(action, (conn, handler) => conn.PropertyInspectorDidAppear += handler, args => args.Context);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.PropertyInspectorDidDisappear"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.PropertyInspectorDidDisappear"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapPropertyInspectorDidDisappear(this IHost host, Delegate action)
+            => host.MapEvent<ActionEventArgs>(action, (conn, handler) => conn.PropertyInspectorDidDisappear += handler, args => args.Context);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.SendToPlugin"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="PartialActionEventArgs{JsonObject}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.SendToPlugin"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapSendToPlugin(this IHost host, Delegate action)
+            => host.MapEvent<PartialActionEventArgs<JsonObject>>(action, (conn, handler) => conn.SendToPlugin += handler, args => args.Context);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.SystemDidWakeUp"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="StreamDeckEventArgs"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.SystemDidWakeUp"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapSystemDidWakeUp(this IHost host, Delegate action)
+            => host.MapEvent<StreamDeckEventArgs>(action, (conn, handler) => conn.SystemDidWakeUp += handler);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.TitleParametersDidChange"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs{TitlePayload}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.TitleParametersDidChange"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapTitleParametersDidChange(this IHost host, Delegate action)
+            => host.MapEvent<ActionEventArgs<TitlePayload>>(action, (conn, handler) => conn.TitleParametersDidChange += handler, args => args.Context);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.WillAppear"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs{KeyPayload}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.WillAppear"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapWillAppear(this IHost host, Delegate action)
+            => host.MapEvent<ActionEventArgs<ActionPayload>>(action, (conn, handler) => conn.WillAppear += handler, args => args.Context);
+
+        /// <summary>
+        /// Maps the delegate to the <see cref="IStreamDeckConnection.WillDisappear"/> event. When invoked, parameters are resolved from the host's <see cref="IServiceProvider"/>,
+        /// except <see cref="IStreamDeckConnection"/> and <see cref="ActionEventArgs{ActionPayload}"/> which are propagated directly from the event.
+        /// </summary>
+        /// <param name="host">The <see cref="IHost"/> to configure.</param>
+        /// <param name="action">The action to map to <see cref="IStreamDeckConnection.WillDisappear"/>.</param>
+        /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
+        public static IHost MapWillDisappear(this IHost host, Delegate action)
+            => host.MapEvent<ActionEventArgs<ActionPayload>>(action, (conn, handler) => conn.WillDisappear += handler, args => args.Context);
+
+        /// <summary>
+        /// Compiles the delegate, and invokes <paramref name="addHandler"/> allowing the event handler to be added to the <see cref="IStreamDeckConnection"/>.
         /// </summary>
         /// <typeparam name="TArgs">The type of the event arguments.</typeparam>
         /// <param name="host">The <see cref="IHost"/> to configure.</param>
         /// <param name="action">The action that will be invoked by the <see cref="IStreamDeckConnection"/>.</param>
-        /// <param name="addHandler">The delegate responsible for mapping the <paramref name="action"/> to the <see cref="IStreamDeckConnection"/>.</param>
+        /// <param name="addHandler">The delegate responsible for mapping the delegate to the <see cref="IStreamDeckConnection"/>.</param>
         /// <param name="getContext">The optional factory for selecting the context from the <typeparamref name="TArgs"/>; this is supplied to the <see cref="IDispatcher.Invoke(Func{Task}, string)"/>.</param>
         /// <returns>The same instance of the <see cref="IHost"/> for chaining.</returns>
         private static IHost MapEvent<TArgs>(this IHost host, Delegate action, Action<IStreamDeckConnection, EventHandler<IStreamDeckConnection, TArgs>> addHandler, Func<TArgs, string>? getContext = null)
--- tests/StreamDeck.Extensions.Tests/Extensions/Hosting/HostExtensionsTests.cs
+++ tests/StreamDeck.Extensions.Tests/Extensions/Hosting/HostExtensionsTests.cs
@@ -1,5 +1,6 @@
 namespace StreamDeck.Tests.Extensions.Hosting
 {
+    using System.Text.Json.Nodes;
     using Microsoft.Extensions.Hosting;
     using StreamDeck.Events;
     using StreamDeck.Extensions.Hosting;
@@ -13,32 +14,216 @@ namespace StreamDeck.Tests.Extensions.Hosting
     public class HostExtensionsTests
     {
         /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapConnection(IHost, Action{IStreamDeckConnection})"/> correctly calls the delegate enabling the configuring of the <see cref="IStreamDeckConnection"/>.
+        /// </summary>
+        [Test]
+        public void MapConnection()
+        {
+            // Arrange.
+            IStreamDeckConnection? actualConnection = null;
+            var (host, _, connection, _, _) = CreateTestCase();
+
+            // Act.
+            host.MapConnection(c => actualConnection = c);
+
+            // Assert.
+            Assert.That(actualConnection, Is.EqualTo(connection.Object));
+        }
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapApplicationDidLaunch(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapApplicationDidLaunch()
+            => VerifyMap(
+                (host, handler) => host.MapApplicationDidLaunch(handler),
+                conn => conn.ApplicationDidLaunch += null,
+                args: new StreamDeckEventArgs<ApplicationPayload>("foo", new ApplicationPayload("notepad.exe")),
+                context: string.Empty);
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapApplicationDidTerminate(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapApplicationDidTerminate()
+            => VerifyMap(
+                (host, handler) => host.MapApplicationDidTerminate(handler),
+                conn => conn.ApplicationDidTerminate += null,
+                args: new StreamDeckEventArgs<ApplicationPayload>("foo", new ApplicationPayload("notepad.exe")),
+                context: string.Empty);
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapDeviceDidConnect(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapDeviceDidConnect()
+            => VerifyMap(
+                (host, handler) => host.MapDeviceDidConnect(handler),
+                conn => conn.DeviceDidConnect += null,
+                args: new DeviceConnectEventArgs("event", "device", new DeviceInfo("name", new Size(1, 1), Device.StreamDeckXL)),
+                context: string.Empty);
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapDeviceDidDisconnect(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapDeviceDidDisconnect()
+            => VerifyMap(
+                (host, handler) => host.MapDeviceDidDisconnect(handler),
+                conn => conn.DeviceDidDisconnect += null,
+                args: new DeviceEventArgs("event", "device"),
+                context: string.Empty);
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapDidReceiveGlobalSettings(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapDidReceiveGlobalSettings()
+            => VerifyMap(
+                (host, handler) => host.MapDidReceiveGlobalSettings(handler),
+                conn => conn.DidReceiveGlobalSettings += null,
+                args: new StreamDeckEventArgs<SettingsPayload>("event", new SettingsPayload(new System.Text.Json.Nodes.JsonObject())),
+                context: string.Empty);
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapDidReceiveSettings(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapDidReceiveSettings()
+            => VerifyMap(
+                (host, handler) => host.MapDidReceiveSettings(handler),
+                conn => conn.DidReceiveSettings += null,
+                args: EventArgsBuilder.CreateActionEventArgs(context: "ABC123"),
+                context: "ABC123");
+
+        /// <summary>
         /// Asserts the <see cref="HostExtensions.MapKeyDown(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
         /// </summary>
         [Test]
         public void MapKeyDown()
+            => VerifyMap(
+                (host, handler) => host.MapKeyDown(handler),
+                conn => conn.KeyDown += null,
+                args: EventArgsBuilder.CreateKeyEventArgs(context: "ABC123"),
+                context: "ABC123");
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapKeyUp(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapKeyUp()
+            => VerifyMap(
+                (host, handler) => host.MapKeyUp(handler),
+                conn => conn.KeyUp += null,
+                args: EventArgsBuilder.CreateKeyEventArgs(context: "ABC123"),
+                context: "ABC123");
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapPropertyInspectorDidAppear(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapPropertyInspectorDidAppear()
+            => VerifyMap(
+                (host, handler) => host.MapPropertyInspectorDidAppear(handler),
+                conn => conn.PropertyInspectorDidAppear += null,
+                args: new ActionEventArgs("event", "action", "ABC123", "device"),
+                context: "ABC123");
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapPropertyInspectorDidDisappear(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapPropertyInspectorDidDisappear()
+            => VerifyMap(
+                (host, handler) => host.MapPropertyInspectorDidDisappear(handler),
+                conn => conn.PropertyInspectorDidDisappear += null,
+                args: new ActionEventArgs("event", "action", "ABC123", "device"),
+                context: "ABC123");
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapSendToPlugin(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapSendToPlugin()
+            => VerifyMap(
+                (host, handler) => host.MapSendToPlugin(handler),
+                conn => conn.SendToPlugin += null,
+                args: new PartialActionEventArgs<JsonObject>("event", new JsonObject(), "action", "ABC123"),
+                context: "ABC123");
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapSystemDidWakeUp(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapSystemDidWakeUp()
+            => VerifyMap(
+                (host, handler) => host.MapSystemDidWakeUp(handler),
+                conn => conn.SystemDidWakeUp += null,
+                args: new StreamDeckEventArgs("event"),
+                context: string.Empty);
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapTitleParametersDidChange(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapTitleParametersDidChange()
+            => VerifyMap(
+                (host, handler) => host.MapTitleParametersDidChange(handler),
+                conn => conn.TitleParametersDidChange += null,
+                args: EventArgsBuilder.CreateTitleEventArgs(context: "ABC123"),
+                context: "ABC123");
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapWillAppear(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapWillAppear()
+            => VerifyMap(
+                (host, handler) => host.MapWillAppear(handler),
+                conn => conn.WillAppear += null,
+                args: EventArgsBuilder.CreateActionEventArgs(context: "ABC123"),
+                context: "ABC123");
+
+        /// <summary>
+        /// Asserts the <see cref="HostExtensions.MapWillDisappear(IHost, Delegate)"/> correctly resolves all arguments, the delegate is invoked, and it is invoked on the dispatcher.
+        /// </summary>
+        [Test]
+        public void MapWillDisappear()
+            => VerifyMap(
+                (host, handler) => host.MapWillDisappear(handler),
+                conn => conn.WillDisappear += null,
+                args: EventArgsBuilder.CreateActionEventArgs(context: "ABC123"),
+                context: "ABC123");
+
+        /// <summary>
+        /// Verifies a mapping on the <see cref="IHost"/>.
+        /// </summary>
+        /// <typeparam name="TArgs">The type of the arguments.</typeparam>
+        /// <param name="map">The action responsible for applying the mapping.</param>
+        /// <param name="eventExpression">The event expression used by <see cref="IMock{IStreamDeckConnection}"/> to raise the event.</param>
+        /// <param name="args">The arguments supplied to the event when raising it.</param>
+        /// <param name="context">The expected context supplied to the <see cref="IDispatcher.Invoke(Func{Task}, string)"/>.</param>
+        private static void VerifyMap<TArgs>(Action<IHost, Delegate> map, Action<IStreamDeckConnection> eventExpression, TArgs args, string context)
         {
             // Arrange.
             var wasInvoked = false;
-
             var (host, serviceProvider, connection, dispatcher, service) = CreateTestCase();
-            var eventArgs = EventArgsBuilder.CreateKeyEventArgs();
 
             // Act.
-            host.MapKeyDown(Handler);
-            connection.Raise(c => c.KeyDown += null, connection.Object, eventArgs);
+            map(host, Handler);
+            connection.Raise(eventExpression, connection.Object, args);
 
             // Assert.
             Assert.That(wasInvoked, Is.True);
-            dispatcher.Verify(d => d.Invoke(It.IsAny<Func<Task>>(), eventArgs.Context), Times.Once);
+            dispatcher.Verify(d => d.Invoke(It.IsAny<Func<Task>>(), context), Times.Once);
 
-            void Handler(IStreamDeckConnection sender, ActionEventArgs<KeyPayload> args, IService resolvedService)
+            void Handler(IStreamDeckConnection sender, TArgs actualArgs, IService resolvedService)
             {
                 wasInvoked = true;
                 Assert.Multiple(() =>
                 {
                     Assert.That(sender, Is.EqualTo(connection.Object));
-                    Assert.That(args, Is.EqualTo(eventArgs));
+                    Assert.That(actualArgs, Is.EqualTo(args));
                     Assert.That(resolvedService, Is.EqualTo(service));
                 });
             }
